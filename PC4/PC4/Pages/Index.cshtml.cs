178e05aee082018bb7bfe2b7019a15dbd7179bfe
ArthurAlg <aliagaarturo2004@gmail.com@>
2023-11-08 23:10:11 -0500

    Primer Commit

---
+++ PC4/PC4/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using PC4;
+
+namespace PC4.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly MLModel _mLModel;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _mLModel = new MLModel();
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost(string palabra)
+        {
+            if (!string.IsNullOrEmpty(palabra))
+            {
+                var input = new MLModel.ModelInput
+                {
+                    Col0 = palabra
+                };
+                var prediction = MLModel.Predict(input);
+
+                string resultado = prediction.PredictedLabel == 1 ? "Positive" : "Negative";
+
+                ViewData["Resultado"] = $"The word '{palabra}' is: {resultado}";
+
+                return Page();
+            }
+
+            return Page();
+        }
+    }
+}
